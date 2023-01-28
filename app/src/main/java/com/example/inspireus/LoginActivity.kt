775979fefc710b77d97757d84098a3593d515cfe
Juan Alegre <jpalegree@gmail.com>
2023-01-27 23:33:59 -0300

    refactor LoginActivity.kt

--- app/src/main/java/com/example/inspireus/LoginActivity.kt
+++ app/src/main/java/com/example/inspireus/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.inspireus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.inspireus.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -12,5 +13,8 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_login) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
